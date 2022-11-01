1798e952115c5847b554e97dd07681d461dacc8d
Alexandr <jwchaganav@gmail.com>
2022-11-01 15:02:11 +0400

    HelloWord

--- Controllers/HelloWordController.cs
+++ Controllers/HelloWordController.cs
@@ -1,18 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using Inventory.Models;
 
-namespace Inventory.Controllers
+namespace Inventory.Controllers;
+
+public class HelloWordController : Controller
 {
-    public class HelloWordContrloller : Controller
+    public string Index()
     {
-        public string Index()
-        {
-            return "Этот метод по умолчанию";
-        }
+        return "Этот метод по умолчанию";
+    }
 
-        public string Welcome()
-        {
-            return "Этот метод Welcome";
-        }
+    public string Welcome()
+    {
+        return "Этот метод Welcome";
     }
 }
