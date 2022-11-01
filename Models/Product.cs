01b4b3f606ff3d8d1a5750166554553af1a363ab
Alexandr <jwchaganav@gmail.com>
2022-11-01 12:27:12 +0400

    Product

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+namespace Inventory.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set;}
+        public string Name { get; set;}
+        public DateTime DateProiz {get; set;}
+        
+        public string Bios {get; set;}
+        public string DallasAdmin {get; set;}
+        public string DallasOper {get; set;}
+        public int DoctorID {get; set;}
+        public string Description {get; set;}
+        public string Category {get; set;}
+        public int DepartmentID {get; set;}
+    }
+}
