00cc9bebaf8cfc9432eaade2516b145af5daa67e
Alexandr <jwchaganav@gmail.com>
2022-11-03 14:37:29 +0400

    Department

---
+++ Migrations/20221103083037_ReleaseDate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Migrations
+{
+    public partial class ReleaseDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateProiz",
+                table: "Product",
+                newName: "ReleaseDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Product",
+                newName: "DateProiz");
+        }
+    }
+}
