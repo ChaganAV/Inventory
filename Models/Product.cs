ea0894b76e66765a9f4d5434f08a7cbfb5ea8d9e
Alexandr <jwchaganav@gmail.com>
2022-11-04 12:58:57 +0400

    annotation

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,22 +6,33 @@ namespace Inventory.Models
     public class Product
     {
         public int ProductID { get; set;}
+
+        [Display(Name = "Наименование")]
         public string? Name { get; set;}
+
+        [Display(Name = "Серийный номер")]
         public string? Serial {get; set;}
 
+        [Display(Name = "Инвентарный номер")]
         public string? InvNumber {get; set;}
 
         [Display(Name = "Дата изготовления")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
         
+        [Display(Name = "Биос")]
         public string? Bios {get; set;}
         public string? DallasAdmin {get; set;}
         public string? DallasOper {get; set;}
         public int DoctorID {get; set;}
         //public string? Doctor {get; set;}
+
+        [Display(Name = "Описание")]
         public string? Description {get; set;}
+
+        [Display(Name = "Категория")]
         public string? Category {get; set;}
+        
         public int DepartmentID {get; set;}
         //public string? Department {get; set;}
     }
