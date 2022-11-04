c6c07df258185913236f4d0e6a8b1c35571d2f48
Alexandr <jwchaganav@gmail.com>
2022-11-04 12:39:38 +0400

    database update добавил column InvNumber

---
+++ Migrations/20221104083654_AnnotationData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Migrations
+{
+    public partial class AnnotationData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvNumber",
+                table: "Product",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvNumber",
+                table: "Product");
+        }
+    }
+}
